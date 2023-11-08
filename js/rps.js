cc41e8a1e7e14394ae46d76ea1992a1e9b524244
Quilhar <davidtit240@gmail.com>
2023-11-08 12:32:51 -0500

    commit

--- js/rps.js
+++ js/rps.js
@@ -49,14 +49,19 @@
 const rockButton = document.getElementById('rock')
 const paperButton = document.getElementById('paper')
 const scissorsButton = document.getElementById('scissors')
+const resetButton = document.getElementById('newgame')
 const result = document.querySelector('.result')
 const userImg = document.querySelector('.user-img')
 const compImg = document.querySelector('.comp-img')
-const  = document.querySelector('.result')
+const userTrack = document.querySelector('.user-score')
+const compTrack = document.querySelector('.comp-score')
+const winner = document.querySelector('.final-winner')
+const header = document.querySelector('.header-con')
 
 const choices = ["rock", "paper", "scissors"]
 let comp
 let userScore = 0
+let compScore = 0
 
 // function rockBtnClick() {
 //     // to change the text of the result object
@@ -77,18 +82,19 @@ rockButton.addEventListener('click', () => {
     // result.style.backroundColor = "red" (Changing an objects styles by using .style. followed by the style)
     compChoice()
     winLose(rockButton.id)
-    userScore = userScore + 1
-    .innertext = userScore.toString ()
+    console.log(compScore, userScore)
 })
 paperButton.addEventListener('click', () => {
     userImg.src = "../img/rps_imgs/" + paperButton.id + ".png"
     compChoice()
     winLose(paperButton.id)
+    console.log(compScore, userScore)
 })
 scissorsButton.addEventListener('click', () => {
     userImg.src = "../img/rps_imgs/" + scissorsButton.id + ".png"
     compChoice()
     winLose(scissorsButton.id)
+    console.log(compScore, userScore)
 })
 
 // getting a random number to decide the index for the computer choice
@@ -96,27 +102,64 @@ function compChoice () {
     const randNum = Math.floor(Math.random()*3)
     comp = choices[randNum]
     compImg.src = "../img/rps_imgs/" + comp + ".png"
+
 }
 
 function winLose(userChoice) {
     if (userChoice == "rock" && comp == "paper") {
         result.innerText = "Computer Wins"
+        compScore = compScore + 1
+        compTrack.innerText = compScore.toString()
     } else if (userChoice == "scissors" && comp == "rock") {
        result.innerText = "Computer Wins"
+        compScore = compScore + 1
+        compTrack.innerText = compScore.toString()
     } else if (userChoice == "paper" && comp == "scissors") {
         result.innerText = "Computer Wins"
+        compScore = compScore + 1
+        compTrack.innerText = compScore.toString()
     } else if (comp == "scissors" && userChoice == "rock") {
         result.innerText = "User Wins"
+        userScore = userScore + 1
+        userTrack.innerText = userScore.toString()
     } else if (comp == "paper" && userChoice == "scissors") {
         result.innerText = "User Wins"
+        userScore = userScore + 1
+        userTrack.innerText = userScore.toString()
     } else if (comp == "scissors" && userChoice == "rock") {
         result.innerText = "User Wins"  
+        userScore = userScore + 1
+        userTrack.innerText = userScore.toString()
     } else {
         result.innerText = "Tie"
     }
+    finalWinner()
 }
 
-function scoreTracker() {
- 
-
+function reset() {
+    userScore = 0
+    userTrack.innerText = "0"
+    compScore = 0
+    compTrack.innerText = "0"
+    result.innerText = "Start Game"
+    userImg.src = "../img/rps_imgs/rps4.png"
+    compImg.src = "../img/rps_imgs/rps4.png"
+    header.style.display = "none"
 }
+
+resetButton.addEventListener('click', () => {
+    reset()
+    console.log(compScore, userScore)
+})
+
+function finalWinner() {
+    if (userScore == 3) {
+        winner.innerText = "YOU WIN"
+        header.style.display = "block"
+    } else if (compScore == 3) {
+        winner.innerText = "YOU LOSE"
+        header.style.display = "block"
+    }
+    
+}
+
