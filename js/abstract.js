347b2262102a72339d80a6b9a1c6d3549fa77b86
Quilhar <davidtit240@gmail.com>
2023-12-06 12:44:31 -0500

    commit

--- js/abstract.js
+++ js/abstract.js
@@ -1,5 +1,11 @@
 const signInButton = document.getElementById('signin')
 
-signInButton.addEventListener("mouseover" () => {
-    signInButton.style.backroundColor = "white"
+signInButton.addEventListener("mouseover", () => {
+    signInButton.style.backgroundColor = "white"
+    signInButton.style.color = "black"
+})
+
+signInButton.addEventListener("mouseleave", () => {
+    signInButton.style.backgroundColor = "#4C5FD5"
+    signInButton.style.color = "white"
 })
